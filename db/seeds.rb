b6eb6562c646dadb3cb6c32f3107a338152f992c
John <jvansch1@binghamton.edu>
2019-06-25 20:31:35 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Video.create(name: "Test Video", description: 'Just testing nbd', s3_url: '"https://jvs-video-dev.s3.amazonaws.com/Waterfall+-+6998.mp4"')
+Video.create(name: "Second Video", description: 'Addining a video so theres more than one on the index', s3_url: '"https://jvs-video-dev.s3.amazonaws.com/Waterfall+-+6998.mp4"')
