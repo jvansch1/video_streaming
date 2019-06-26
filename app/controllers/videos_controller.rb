de433d3a9ce2a59db94eedbb20b6d6f8c5e18c15
John <jvansch1@binghamton.edu>
2019-06-25 20:23:37 -0400

    Add index page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,11 @@ require 'aws-sdk'
 require 'json'
 
 class VideosController < ApplicationController
+
+  def index
+    @videos = Video.all
+  end
+
   def show
     @video_record = Video.find(params[:id])
     # s3_client = Aws::S3::Client.new(
@@ -10,6 +15,7 @@ class VideosController < ApplicationController
     #   access_key_id: 'AKIA4OQZ2XG46I7I6B23',
     #   secret_access_key: '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'
     # )
+    @video_url = @video_record.s3_url
     # @video = s3_client.get_object({
     #   bucket: 'jvs-video-dev',
     #   key: 'Waterfall - 6998.mp4',
