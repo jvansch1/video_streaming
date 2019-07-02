d61e780656ad9ec15c50a0f4e5ecdee5b9c6a532
John <jvansch1@binghamton.edu>
2019-07-01 21:10:01 -0400

    Add encoded credentials

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,16 +4,16 @@ require 'json'
 class VideosController < ApplicationController
   def show
     @video_record = Video.find(params[:id])
-    # s3_client = Aws::S3::Client.new(
-    #   profile: 'john',
-    #   region: 'us-east-1',
-    #   access_key_id: 'AKIA4OQZ2XG46I7I6B23',
-    #   secret_access_key: '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'
-    # )
-    # @video = s3_client.get_object({
-    #   bucket: 'jvs-video-dev',
-    #   key: 'Waterfall - 6998.mp4',
-    # })
+    s3_client = Aws::S3::Client.new(
+      profile: 'john',
+      region: 'us-east-1',
+      access_key_id: Rails.application.credentials.aws[:access_key_id],
+      secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+    )
+    @video = s3_client.get_object({
+      bucket: 'jvs-video-dev',
+      key: 'Waterfall - 6998.mp4',
+    })
   end
 
 end
