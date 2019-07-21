8ced6e5459968ca4a1454068690e292e72c5884a
John <jvansch1@binghamton.edu>
2019-07-21 18:48:05 -0400

    Get react router working with home link

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home.jsx'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App.jsx'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
